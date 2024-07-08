761be032240dc4ae59794ed1a48f9c0fe98c0ed7
4ytk3 <66790642+4ytk3@users.noreply.github.com>
2024-07-08 23:54:57 +0900

    create: font.sh

---
+++ init/font.sh
@@ -0,0 +1,5 @@
+sudo wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip
+unzip HackGen_NF_v2.9.0.zip
+mv HackGen_NF_バージョン HackGen_NF
+cp -r HackGen_NF ~/.local/share/fonts
+fc-cache -vf
