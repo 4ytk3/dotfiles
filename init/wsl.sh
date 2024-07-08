0160e801f62809dadd6768830e4616808b645dcd
4ytk3 <66790642+4ytk3@users.noreply.github.com>
2024-07-08 23:54:30 +0900

    update: wsl.sh

--- init/wsl.sh
+++ init/wsl.sh
@@ -1,1 +1,9 @@
-
+sudo apt install curl -y
+sudo apt install git -y
+sudo apt install zsh -y
+sudo apt install xsel -y
+sudo apt install wget -y
+sudo apt install zip unzip -y
+sudo apt install tmux -y
+sudo apt install vim -y
+sudo apt install fzf -y
