183a2000510ed89316847bd17de32ef5afdf6313
4ytk3 <66790642+4ytk3@users.noreply.github.com>
2024-07-08 12:33:24 +0900

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,34 @@
-DOT_DIR="$home"
-# clone
-git clone https://github.com/4ytk3/dotfiles.git ${DOT_DIR}
-# link
 #!/bin/bash
-DOT_DIR="$HOME/dotfiles"
-for f in *;
-do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".gitignore" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-    [[ "$f" == "README.md" ]] && continue
-    [[ "$f" == "install.sh" ]] && continue
 
-    ln -snf $DOT_DIR/"$f" $HOME/".$f"
-    echo "Installed .$f"
-done
+DOT_DIR="$HOME/dotfiles"
+
+has() {
+    type "$1" > /dev/null 2>&1
+}
+
+if if [ ! -d ${DOT_DIR} ]; then
+    if has "git"; then
+        git clone https://github.com/4ytk3/dotfiles.git ${DOT_DIR}
+    else
+        echo "git required"
+        exit 1
+    fi
+    cd ${DOT_DIR}
+    for f in *
+    do
+        [[ "$f" == ".git" ]] && continue
+        [[ "$f" == ".gitignore" ]] && continue
+        [[ "$f" == ".DS_Store" ]] && continue
+        [[ "$f" == "README.md" ]] && continue
+        [[ "$f" == "install.sh" ]] && continue
+
+        ln -snf $DOT_DIR/"$f" $HOME/".$f"
+        echo "Installed .$f"
+    done
+else
+    echo "dotfiles already exists"
+    exit 1
+fi
 
 # Macなら普通のbrew、WSLならLinux-brewを入れる処理をする箇所
 if [ "$(uname)" == 'Darwin' ]; then
@@ -35,5 +49,3 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
 else
   exit 1
 fi
-
-source ~/.zshrc
