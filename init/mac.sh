c60275140cd6f97d7f72da8f76cb8743bae863d2
4ytk3 <66790642+4ytk3@users.noreply.github.com>
2024-07-08 14:05:14 +0900

    add: mac.sh

--- init/mac.sh
+++ init/mac.sh
@@ -1,1 +1,12 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ "$(uname -m)" = "arm64" ] ; then
+  eval "$(/opt/homebrew/bin/brew shellenv)" > /dev/null
+fi
 
+brew install zsh
+sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+chsh -s /usr/local/bin/zsh
+
+brew bundle --global
+
+echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
