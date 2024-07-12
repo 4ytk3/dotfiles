1fbc5627c156613857405fc35d2595f1ed6f74a7
4ytk3 <nuts13718@gmail.com>
2024-07-12 17:17:24 +0900

    update: zshrc

--- init/font.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/.local/share/fonts
-sudo wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip
-unzip HackGen_NF_v2.9.0.zip
-mv HackGen_NF_v2.9.0 HackGen_NF
-mv HackGen_NF ~/.local/share/fonts
-fc-cache -vf
-rm -rf HackGen_NF_v2.9.0.zip
