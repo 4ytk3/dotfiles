d8b1f94f6e11e237d4ac8170569131e93681617b
4ytk3 <nuts13718@gmail.com>
2024-07-12 16:01:12 +0900

    update: wsl.sh

--- init/wsl.sh
+++ init/wsl.sh
@@ -7,3 +7,12 @@ sudo apt install zip unzip -y
 sudo apt install tmux -y
 sudo apt install vim -y
 sudo apt install fzf -y
+
+# font install
+mkdir -p ~/.local/share/fonts
+sudo wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip
+unzip HackGen_NF_v2.9.0.zip
+mv HackGen_NF_v2.9.0 HackGen_NF
+mv HackGen_NF ~/.local/share/fonts
+fc-cache -vf
+rm -rf HackGen_NF_v2.9.0.zip
