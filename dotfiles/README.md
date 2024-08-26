761f1bd17ded2c6f0b06d207e6c15210bc9da686
4ytk3 <66790642+4ytk3@users.noreply.github.com>
2024-08-26 17:47:39 +0900

    delete: dotfiles directory

--- dotfiles/README.md
+++
@@ -1,2 +0,0 @@
-# dotfiles
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/4ytk3/dotfiles/main/install.sh)"
--- dotfiles/init/zsh_plugins.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/olets/zsh-abbr --single-branch --branch main --depth 1 ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-abbr
